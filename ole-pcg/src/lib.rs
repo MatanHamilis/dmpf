0a46c1f37139cf30010daf6e8e0a3935e94b0dd5
matan hamilis <matan.hamilis@gmail.com>
2024-02-04 20:11:07 +0200

    worked regular

--- ole-pcg/src/lib.rs
+++ ole-pcg/src/lib.rs
@@ -1,16 +1,17 @@
 use std::{
-    collections::HashSet,
     ops::{Index, IndexMut, Mul},
     time::Instant,
 };
 
 use aes_prng::AesRng;
 use dmpf::{Dmpf, RadixTwoFftFriendFieldElement, SmallFieldContainer};
-use polynomial::{share_polynomial, DensePolynomial, SparsePolynomial};
+use polynomial::{
+    share_polynomial, share_regular_polynomial_mul, DensePolynomial, SparsePolynomial,
+};
 use rand::{thread_rng, RngCore, SeedableRng};
 use ring::{ModuloPolynomial, PolynomialRingElement, SparsePolynomialRingElement};
 
-use crate::{fft::fft, polynomial::from_share};
+use crate::polynomial::{from_share, from_share_regular_error};
 
 mod fft;
 mod polynomial;
@@ -99,11 +100,92 @@ impl<
             .sum();
         println!("Additive share gen: {}", time.elapsed().as_millis());
         (multiplicative_share_poly, additive_share_poly)
-        // let additive_shares = fft(&additive_share_poly.get_coefficients().coefficients);
-        // multiplicative_shares
-        //     .into_iter()
-        //     .zip(additive_shares.into_iter())
-        //     .collect()
+    }
+}
+
+pub struct OlePcgSeedRegular<
+    const CONTAINER_SIZE: usize,
+    F: RadixTwoFftFriendFieldElement,
+    C: SmallFieldContainer<CONTAINER_SIZE, F>,
+    D: Dmpf<C>,
+    M: ModuloPolynomial<F>,
+> {
+    my_vec_seed: [u8; 16],
+    peer_vec_seed: [u8; 16],
+    role: Role,
+    compression_factor: usize,
+    sparse_polynomials: Vec<SparsePolynomialRingElement<F, M>>,
+    tensor_product_fss: Vec<(usize, Vec<(usize, D::Key)>)>,
+}
+impl<
+        const CONTAINER_SIZE: usize,
+        F: RadixTwoFftFriendFieldElement,
+        C: SmallFieldContainer<CONTAINER_SIZE, F>,
+        D: Dmpf<C>,
+        M: ModuloPolynomial<F>,
+    > OlePcgSeedRegular<CONTAINER_SIZE, F, C, D, M>
+{
+    fn new(
+        my_vec_seed: [u8; 16],
+        peer_vec_seed: [u8; 16],
+        role: Role,
+        compression_factor: usize,
+        sparse_polynomials: Vec<SparsePolynomialRingElement<F, M>>,
+        tensor_product_fss: Vec<(usize, Vec<(usize, D::Key)>)>,
+    ) -> Self {
+        Self {
+            my_vec_seed,
+            peer_vec_seed,
+            role,
+            compression_factor,
+            sparse_polynomials,
+            tensor_product_fss,
+        }
+    }
+    fn expand(&self) -> (PolynomialRingElement<F, M>, PolynomialRingElement<F, M>) {
+        // First, compute the multiplicative shares:
+        let my_polynomials = polynomials_from_seed(
+            self.my_vec_seed,
+            self.sparse_polynomials[0].modulo().clone(),
+            self.compression_factor,
+        );
+        let peer_polynomials = polynomials_from_seed(
+            self.peer_vec_seed,
+            self.sparse_polynomials[0].modulo().clone(),
+            self.compression_factor,
+        );
+        let time = Instant::now();
+        assert_eq!(my_polynomials.len(), self.sparse_polynomials.len());
+        let multiplicative_share_poly: PolynomialRingElement<F, M> = my_polynomials
+            .iter()
+            .zip(self.sparse_polynomials.iter())
+            .map(|(p, s)| s * p)
+            .sum();
+        println!("Multiplicative share gen: {}", time.elapsed().as_millis());
+
+        // Second, compute additive shares:
+        let tensor_product_dense = match self.role {
+            Role::First => tensor_product(&my_polynomials, &peer_polynomials),
+            Role::Second => tensor_product(&peer_polynomials, &my_polynomials),
+        };
+        let time = Instant::now();
+        let additive_share_poly: PolynomialRingElement<F, M> = self
+            .tensor_product_fss
+            .iter()
+            .zip(tensor_product_dense.v.iter())
+            .map(|((k_0, k_1), p)| {
+                let dense_poly = from_share_regular_error::<CONTAINER_SIZE, F, C, D>(
+                    (*k_0, &k_1[..]),
+                    self.sparse_polynomials[0].modulo().deg() * 2 - 1,
+                );
+                let dense_poly = p.get_modulo().modulo(dense_poly);
+                let dense_ring_element =
+                    PolynomialRingElement::new(dense_poly, p.get_modulo().clone());
+                p * &dense_ring_element
+            })
+            .sum();
+        println!("Additive share gen: {}", time.elapsed().as_millis());
+        (multiplicative_share_poly, additive_share_poly)
     }
 }
 
@@ -119,6 +201,18 @@ fn polynomials_from_seed<F: RadixTwoFftFriendFieldElement, M: ModuloPolynomial<F
             let p = DensePolynomial::<F>::from_iter((0..degree).map(|_| F::random(&mut rng)));
             PolynomialRingElement::new(p, modulo_polynomial.clone())
         })
+        .collect()
+}
+
+fn sparse_regular_polynomials_from_seed<F: RadixTwoFftFriendFieldElement>(
+    seed: [u8; 16],
+    weight: usize,
+    degree: usize,
+    polyomials_count: usize,
+) -> Vec<SparsePolynomial<F>> {
+    let mut rng = AesRng::from_seed(seed);
+    (0..polyomials_count)
+        .map(|_| SparsePolynomial::random_regular(&mut rng, degree, weight))
         .collect()
 }
 
@@ -168,6 +262,76 @@ where
     Matrix::new(output, b.len())
 }
 
+fn gen_regular<
+    const CONTAINER_SIZE: usize,
+    F: RadixTwoFftFriendFieldElement,
+    C: SmallFieldContainer<CONTAINER_SIZE, F>,
+    D: Dmpf<C>,
+    M: ModuloPolynomial<F>,
+>(
+    log_polynomial_degree: usize,
+    compression_factor: usize,
+    modulo_polynomial: M,
+    weight: usize,
+    dmpf: &D,
+) -> (
+    OlePcgSeedRegular<CONTAINER_SIZE, F, C, D, M>,
+    OlePcgSeedRegular<CONTAINER_SIZE, F, C, D, M>,
+) {
+    let mut rng = thread_rng();
+    let mut first_polynomial_seed = [0u8; 16];
+    // let mut second_polynomial_seed = [0u8; 16];
+    let mut first_sparse_seed = [0u8; 16];
+    let mut second_sparse_seed = [0u8; 16];
+    rng.fill_bytes(&mut first_polynomial_seed);
+    let second_polynomial_seed = first_polynomial_seed;
+    // rng.fill_bytes(&mut second_polynomial_seed);
+    rng.fill_bytes(&mut first_sparse_seed);
+    rng.fill_bytes(&mut second_sparse_seed);
+    let first_sparse_polynomials = sparse_regular_polynomials_from_seed(
+        first_sparse_seed,
+        weight,
+        modulo_polynomial.deg(),
+        compression_factor,
+    );
+    let second_sparse_polynomials = sparse_regular_polynomials_from_seed::<F>(
+        second_sparse_seed,
+        weight,
+        modulo_polynomial.deg(),
+        compression_factor,
+    );
+    let tensor_product = tensor_product(&first_sparse_polynomials, &second_sparse_polynomials);
+    let first_sparse_polynomials: Vec<_> = first_sparse_polynomials
+        .into_iter()
+        .map(|p| SparsePolynomialRingElement::new(p, modulo_polynomial.clone()))
+        .collect();
+    let second_sparse_polynomials: Vec<_> = second_sparse_polynomials
+        .into_iter()
+        .map(|p| SparsePolynomialRingElement::new(p, modulo_polynomial.clone()))
+        .collect();
+    let (first_shares, second_shares): (Vec<_>, Vec<_>) = tensor_product
+        .v
+        .into_iter()
+        .map(|p| share_regular_polynomial_mul(&p, dmpf, weight, log_polynomial_degree + 1))
+        .unzip();
+    let first = OlePcgSeedRegular::new(
+        first_polynomial_seed,
+        second_polynomial_seed,
+        Role::First,
+        compression_factor,
+        first_sparse_polynomials,
+        first_shares,
+    );
+    let second = OlePcgSeedRegular::new(
+        second_polynomial_seed,
+        first_polynomial_seed,
+        Role::Second,
+        compression_factor,
+        second_sparse_polynomials,
+        second_shares,
+    );
+    (first, second)
+}
 fn gen<
     const CONTAINER_SIZE: usize,
     F: RadixTwoFftFriendFieldElement,
@@ -250,7 +414,7 @@ mod tests {
     use rand::thread_rng;
 
     use crate::{
-        gen,
+        gen, gen_regular,
         polynomial::{from_share, share_polynomial, SparsePolynomial},
         ring::{ModuloPolynomial, PolynomialRingElement, TwoPowerDegreeCyclotomicPolynomial},
     };
@@ -268,7 +432,7 @@ mod tests {
         let dense_ab = sparse_ab.to_dense();
         let dense_ab_ring = PolynomialRingElement::new(modulo.modulo(dense_ab), modulo.clone());
 
-        let dmpf = OkvsDmpf::new(WEIGHT * WEIGHT, EpsilonPercent::Ten);
+        let dmpf = OkvsDmpf::new(EpsilonPercent::Ten);
         let (share_a, share_b) = share_polynomial::<2, _, PrimeField64x2, _>(
             &sparse_ab,
             WEIGHT * WEIGHT,
@@ -284,16 +448,47 @@ mod tests {
     }
     #[test]
     fn test_ole_pcg() {
-        const LOG_POLYNOMIAL_DEGREE: usize = 20;
+        const LOG_POLYNOMIAL_DEGREE: usize = 15;
         const COMPRESSION_FACTOR: usize = 2;
         const WEIGHT: usize = 70;
-        // let dmpf = OkvsDmpf::<200, _>::new(WEIGHT * WEIGHT, EpsilonPercent::Ten);
+        let dmpf = OkvsDmpf::<200, _>::new(EpsilonPercent::Ten);
         // let dmpf = DpfDmpf::new();
-        let dmpf = BatchCodeDmpf::new(4, 50);
+        // let dmpf = BatchCodeDmpf::new(4, 50);
         let modulo_polynomial =
             TwoPowerDegreeCyclotomicPolynomial::<PrimeField64>::new(LOG_POLYNOMIAL_DEGREE);
         let time = Instant::now();
         let (first, second) = gen::<2, _, PrimeField64x2, _, _>(
+            LOG_POLYNOMIAL_DEGREE,
+            COMPRESSION_FACTOR,
+            modulo_polynomial,
+            WEIGHT,
+            &dmpf,
+        );
+        println!("OLE PCG took: {}", time.elapsed().as_millis());
+        let time = Instant::now();
+        let first_shares = first.expand();
+        println!("Expand took: {}", time.elapsed().as_millis());
+        let time = Instant::now();
+        let second_shares = second.expand();
+        println!("Expand took: {}", time.elapsed().as_millis());
+
+        assert_eq!(
+            &first_shares.0 * &second_shares.0,
+            &first_shares.1 + &second_shares.1
+        );
+    }
+    #[test]
+    fn test_ole_pcg_regular() {
+        const LOG_POLYNOMIAL_DEGREE: usize = 20;
+        const COMPRESSION_FACTOR: usize = 2;
+        const WEIGHT: usize = 70;
+        // let dmpf = OkvsDmpf::<200, _>::new(EpsilonPercent::Ten);
+        let dmpf = DpfDmpf::new();
+        // let dmpf = BatchCodeDmpf::new(4, 50);
+        let modulo_polynomial =
+            TwoPowerDegreeCyclotomicPolynomial::<PrimeField64>::new(LOG_POLYNOMIAL_DEGREE);
+        let time = Instant::now();
+        let (first, second) = gen_regular::<2, _, PrimeField64x2, _, _>(
             LOG_POLYNOMIAL_DEGREE,
             COMPRESSION_FACTOR,
             modulo_polynomial,
--- ole-pcg/src/polynomial.rs
+++ ole-pcg/src/polynomial.rs
@@ -22,11 +22,11 @@ where
 /// This implements a sparse polynomial represented as coefficients
 #[derive(Debug)]
 pub struct SparsePolynomial<F: RadixTwoFftFriendFieldElement> {
-    coefficients: HashMap<usize, F>,
+    coefficients: Vec<(usize, F)>,
 }
 
 impl<F: RadixTwoFftFriendFieldElement> SparsePolynomial<F> {
-    pub fn new(coefficients: HashMap<usize, F>) -> Self {
+    pub fn new(coefficients: Vec<(usize, F)>) -> Self {
         Self { coefficients }
     }
     pub fn random<R: RngCore + CryptoRng>(mut rng: R, degree: usize, weight: usize) -> Self {
@@ -38,8 +38,25 @@ impl<F: RadixTwoFftFriendFieldElement> SparsePolynomial<F> {
         let coefficients = hash_set.into_iter().map(|idx| (idx, F::random(&mut rng)));
         SparsePolynomial::new(coefficients.collect())
     }
+    pub fn random_regular<R: RngCore + CryptoRng>(
+        mut rng: R,
+        degree_bound: usize,
+        weight: usize,
+    ) -> Self {
+        assert!(degree_bound > weight + 1);
+        let slice_size = degree_bound / weight;
+        let coefficients = (0..weight)
+            .map(|i| {
+                let coeff_deg = ((i * slice_size) + ((rng.next_u32() as usize) % slice_size))
+                    .min(degree_bound - 1);
+                let coeff = F::random(&mut rng);
+                (coeff_deg, coeff)
+            })
+            .collect();
+        SparsePolynomial::new(coefficients)
+    }
     pub fn to_dense(&self) -> DensePolynomial<F> {
-        let deg = self.coefficients.iter().map(|v| *v.0).max().unwrap();
+        let deg = self.coefficients.iter().map(|v| v.0).max().unwrap();
         let mut dense_coefficients = vec![F::zero(); deg + 1];
         self.coefficients
             .iter()
@@ -47,6 +64,100 @@ impl<F: RadixTwoFftFriendFieldElement> SparsePolynomial<F> {
         DensePolynomial {
             coefficients: dense_coefficients,
         }
+    }
+    fn is_regular(&self, degree_max: usize) -> bool {
+        let weight = self.coefficients.len();
+        let block_size = degree_max / weight;
+        self.coefficients
+            .iter()
+            .enumerate()
+            .all(|(i, (c, _))| (*c >= i * block_size) && (*c < i * block_size))
+    }
+}
+// This function takes a polynomial of degree N with at most t*t non-zero points.
+// In particular, this polynomial is a product of two regular, weight t, degree N polynomials.
+// As such, the coefficients of this product polynomial are split into 2*t-1 sets where the i-th set
+// considers contains min(i+1, 2*t-1-i) non-zero points of coefficients x^(N/(2t))
+pub fn share_regular_polynomial_mul<
+    const W: usize,
+    F: RadixTwoFftFriendFieldElement,
+    C: SmallFieldContainer<W, F>,
+    D: Dmpf<C>,
+>(
+    p: &SparsePolynomial<F>,
+    dmpf: &D,
+    sqrt_points: usize,
+    log_degree: usize,
+) -> ((usize, Vec<(usize, D::Key)>), (usize, Vec<(usize, D::Key)>)) {
+    let total_degree: usize = 1 << log_degree;
+    let t = sqrt_points;
+    let block_size = (total_degree / 2) / t;
+    let block_dpf_domain_size = block_size.next_power_of_two();
+    let log_sub_poly_degree = block_dpf_domain_size.ilog2() as usize;
+    let mut total_blocks = 2 * t - 1;
+    if total_blocks * block_size < total_degree {
+        total_blocks += 1;
+    }
+    let mut coefficients_idx = 0;
+    let mut new_poly_to_share = SparsePolynomial::<F>::new(Vec::with_capacity(2 * total_blocks));
+    let mut coefficients_map = HashMap::with_capacity(2 * total_blocks);
+    let (v_0, v_1): (Vec<_>, Vec<_>) = (0..total_blocks)
+        .map(|block_idx| {
+            let block_min_deg = block_idx * block_size;
+            let block_max_deg = (block_idx + 1) * block_size;
+            let expected_block_size = 2 * (block_idx).min(total_blocks - block_idx) + 1;
+            while coefficients_idx < p.coefficients.len()
+                && p.coefficients[coefficients_idx].0 < block_max_deg
+            {
+                let (idx, f) = p.coefficients[coefficients_idx];
+                coefficients_map.insert(idx - block_min_deg, f);
+                coefficients_idx += 1;
+            }
+            assert!(coefficients_map.len() <= expected_block_size);
+            let mut i = block_min_deg;
+            while coefficients_map.len() < expected_block_size && i < block_max_deg {
+                if !coefficients_map.contains_key(&(i - block_min_deg)) {
+                    coefficients_map.insert(i - block_min_deg, F::zero());
+                }
+                i += 1;
+            }
+            coefficients_map
+                .iter()
+                .for_each(|(k, v)| new_poly_to_share.coefficients.push((*k, *v)));
+            new_poly_to_share.coefficients.sort_by_cached_key(|v| v.0);
+            let (k_0, k_1) = share_polynomial(
+                &new_poly_to_share,
+                expected_block_size,
+                dmpf,
+                log_sub_poly_degree,
+            );
+            new_poly_to_share.coefficients.clear();
+            coefficients_map.clear();
+            ((block_min_deg, k_0), (block_min_deg, k_1))
+        })
+        .unzip();
+    ((block_size, v_0), (block_size, v_1))
+}
+pub fn from_share_regular_error<
+    const W: usize,
+    F: RadixTwoFftFriendFieldElement,
+    C: SmallFieldContainer<W, F>,
+    D: Dmpf<C>,
+>(
+    key: (usize, &[(usize, D::Key)]),
+    output_degree: usize,
+) -> DensePolynomial<F> {
+    let mut output = Vec::with_capacity(output_degree);
+    unsafe { output.set_len(output_degree) };
+    let (block_size, blocks) = key;
+    blocks.iter().for_each(|(start_idx, key)| {
+        let p = from_share::<W, F, C, D>(key);
+        for i in 0..block_size.min(output_degree - *start_idx) {
+            output[start_idx + i] = p[i]
+        }
+    });
+    DensePolynomial {
+        coefficients: output,
     }
 }
 pub fn share_polynomial<
@@ -65,13 +176,14 @@ pub fn share_polynomial<
     let lower_bits_to_shrink = W.ilog2() as usize;
     let dpf_input_length = log_degree - lower_bits_to_shrink;
     let mut inputs_hashmap = HashMap::<u128, C>::new();
-    for (&idx, &v) in p.coefficients.iter() {
+    for (idx, v) in p.coefficients.iter() {
         let cur_idx = ((idx >> lower_bits_to_shrink) as u128) << (128 - dpf_input_length);
         let internal_idx = idx & ((1 << lower_bits_to_shrink) - 1);
         let container = inputs_hashmap.entry(cur_idx).or_default();
-        container[internal_idx] = v;
+        container[internal_idx] = *v;
     }
     let mut rng = thread_rng();
+    let points = points.min(1 << dpf_input_length);
     while inputs_hashmap.len() < points {
         let idx = rng.next_u64() % (1 << log_degree);
         let cur_idx = ((idx >> lower_bits_to_shrink) as u128) << (128 - dpf_input_length);
@@ -104,10 +216,20 @@ pub fn from_share<
     DensePolynomial { coefficients }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone)]
 pub struct DensePolynomial<F: RadixTwoFftFriendFieldElement> {
     pub coefficients: Vec<F>,
 }
+impl<F: RadixTwoFftFriendFieldElement> PartialEq for DensePolynomial<F> {
+    fn eq(&self, other: &Self) -> bool {
+        let min_len = self.coefficients.len().min(other.coefficients.len());
+        let first_check = self.coefficients[..min_len] == other.coefficients[..min_len];
+        let second_check = self.coefficients[min_len..].iter().all(|v| v.is_zero());
+        let third_check = other.coefficients[min_len..].iter().all(|v| v.is_zero());
+        first_check && second_check && third_check
+    }
+}
+impl<F: RadixTwoFftFriendFieldElement> Eq for DensePolynomial<F> {}
 
 impl<F: RadixTwoFftFriendFieldElement> DensePolynomial<F> {
     pub fn new(degree_bound: usize) -> Self {
@@ -241,8 +363,8 @@ impl<F: RadixTwoFftFriendFieldElement> Mul<&SparsePolynomial<F>> for &DensePolyn
         let output_len = 2 * self.coefficients.len();
         let mut output = vec![F::zero(); output_len];
         for (idx, c) in self.coefficients.iter().copied().enumerate() {
-            for (&idx_sparse, &c_sparse) in rhs.coefficients.iter() {
-                output[idx + idx_sparse] += c * c_sparse;
+            for (idx_sparse, c_sparse) in rhs.coefficients.iter() {
+                output[idx + idx_sparse] += c * *c_sparse;
             }
         }
         DensePolynomial {
@@ -254,15 +376,18 @@ impl<F: RadixTwoFftFriendFieldElement> Mul<&SparsePolynomial<F>> for &DensePolyn
 impl<F: RadixTwoFftFriendFieldElement> Mul for &SparsePolynomial<F> {
     type Output = SparsePolynomial<F>;
     fn mul(self, rhs: &SparsePolynomial<F>) -> Self::Output {
-        let mut hash_output = HashMap::<usize, F>::new();
-        for (&idx, &c) in self.coefficients.iter() {
-            for (&idx_sparse, &c_sparse) in rhs.coefficients.iter() {
+        let mut hash_output =
+            HashMap::<usize, F>::with_capacity(self.coefficients.len() * rhs.coefficients.len());
+        for (idx, c) in self.coefficients.iter() {
+            for (idx_sparse, c_sparse) in rhs.coefficients.iter() {
                 let v = hash_output.entry(idx + idx_sparse).or_insert(F::zero());
-                *v += c * c_sparse;
+                *v += *c * *c_sparse;
             }
         }
+        let mut coefficients: Vec<_> = hash_output.into_iter().collect();
+        coefficients.sort_by_cached_key(|f| f.0);
         SparsePolynomial {
-            coefficients: hash_output,
+            coefficients: coefficients,
         }
     }
 }
@@ -273,7 +398,9 @@ mod tests {
 
     use crate::{
         fft::{fft, ifft},
-        polynomial::{from_share, share_polynomial, Polynomial, SparsePolynomial},
+        polynomial::{
+            from_share, from_share_regular_error, share_polynomial, Polynomial, SparsePolynomial,
+        },
         ring::TwoPowerDegreeCyclotomicPolynomial,
     };
     use dmpf::{
@@ -286,7 +413,7 @@ mod tests {
     };
     use rand::{thread_rng, RngCore};
 
-    use super::DensePolynomial;
+    use super::{share_regular_polynomial_mul, DensePolynomial};
 
     #[test]
     fn test_fft() {
@@ -324,12 +451,12 @@ mod tests {
         const DEGREE: usize = 1 << LOG_DEGREE;
         const WEIGHT: usize = 2;
         let mut rng = thread_rng();
-        let dmpf = OkvsDmpf::<100, PrimeField64x2>::new(WEIGHT, dmpf::EpsilonPercent::Ten);
+        let dmpf = OkvsDmpf::<100, PrimeField64x2>::new(dmpf::EpsilonPercent::Ten);
         // for _ in 0..TESTS {
         let mut seed = [0u8; 16];
         rng.fill_bytes(&mut seed);
         // let p = SparsePolynomial::random(&mut rng, DEGREE, WEIGHT);
-        let p = SparsePolynomial::new(HashMap::from_iter(
+        let p = SparsePolynomial::new(Vec::from_iter(
             vec![
                 (0, PrimeField64::random(&mut rng)),
                 (1, (PrimeField64::random(&mut rng))),
@@ -343,9 +470,39 @@ mod tests {
         );
         let mut reconstructed = eval_a + eval_b;
         let coeffs = &mut reconstructed.coefficients;
-        p.coefficients.iter().for_each(|(&idx, &f)| {
-            assert_eq!(coeffs[idx], f);
+        p.coefficients.iter().for_each(|(idx, f)| {
+            assert_eq!(coeffs[*idx], *f);
         })
         // }
+    }
+    #[test]
+    fn test_regular_share() {
+        let mut rng = thread_rng();
+        const LOG_DEGREE_BOUND: usize = 9;
+        const DEGREE_BOUND: usize = 1 << LOG_DEGREE_BOUND;
+        const T: usize = 17;
+        let dmpf = DpfDmpf::new();
+        let p = SparsePolynomial::<PrimeField64>::random_regular(&mut rng, DEGREE_BOUND, T);
+        let q = SparsePolynomial::<PrimeField64>::random_regular(&mut rng, DEGREE_BOUND, T);
+        let pq = &p * &q;
+        let (k_0, k_1) = share_regular_polynomial_mul::<2, _, PrimeField64x2, _>(
+            &pq,
+            &dmpf,
+            T,
+            LOG_DEGREE_BOUND + 1,
+        );
+        let reconstructed_first =
+            from_share_regular_error::<2, PrimeField64, PrimeField64x2, DpfDmpf>(
+                (k_0.0, &k_0.1),
+                DEGREE_BOUND << 1,
+            );
+        let reconstructed_second =
+            from_share_regular_error::<2, PrimeField64, PrimeField64x2, DpfDmpf>(
+                (k_1.0, &k_1.1),
+                DEGREE_BOUND << 1,
+            );
+        let sum = &reconstructed_first + &reconstructed_second;
+        let pq_dense = pq.to_dense();
+        assert_eq!(sum, pq_dense);
     }
 }
